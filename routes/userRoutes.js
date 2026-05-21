91ae498431ff04f23da5e877f2d4a161fd1d1719
Priyanka22324 <priyanka22324@gmail.com>
2026-05-20 23:18:27 -0400

    adding server.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -49,4 +49,65 @@ router.post("/register", async (req, res) => {
     }
 });
 
+
+// POST /api/users/login
+router.post("/login", async (req, res) => {
+    try {
+
+        // Get email and password from request body
+        const { email, password } = req.body;
+
+        // Find user by email
+        const user = await User.findOne({ email });
+
+        // If user does not exist
+        if (!user) {
+            return res.status(400).json({
+                message: "Incorrect email or password"
+            });
+        }
+
+        // Compare passwords
+        // Using instance method from model
+        const correctPassword = await user.isCorrectPassword(password);
+
+        // If password is incorrect
+        if (!correctPassword) {
+            return res.status(400).json({
+                message: "Incorrect email or password"
+            });
+        }
+
+        // Create JWT payload
+        const payload = {
+            _id: user._id,
+            username: user.username
+        };
+
+        // Sign token
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+
+        // Send response
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                _id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Server Error",
+            error: error.message
+        });
+    }
+});
+
 module.exports = router;
